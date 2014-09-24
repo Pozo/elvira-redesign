b55110ec37420cd942ccc1ddcf1c1ee6b7f4cc79
Csaba Valyi <csaba.valy1@gmail.com>
2014-09-24 23:07:57 +0200

    unnecessary dependencies removed from MainCtrl

--- app/scripts/mainController.js
+++ app/scripts/mainController.js
@@ -1,33 +1,30 @@
 'use strict';
 
 angular.module('main')
-    .controller('MainCtrl', ['$scope', '$resource', '$datepicker', '$q', 'mainService',
-        function ($scope, $resource, $datepicker, $q, mainService) {
-            this.selectedDate = new Date();
+.controller('MainCtrl', ['$scope', '$datepicker', 'MainService',
+    function ($scope, $datepicker, MainService) {
+        this.selectedDate = new Date();
 
-            this.cities = mav;
+        this.cities = mav;
 
-            this.discounts = [
-                { group : '50%os',id : 1, name: 'asd' },
-                { group : '50%os',id : 2, name: 'bbb' },
-                { group : '90%os',id : 2, name: 'ccc' }
-            ];
+        this.discounts = [
+            { group : '50%os',id : 1, name: 'asd' },
+            { group : '50%os',id : 2, name: 'bbb' },
+            { group : '90%os',id : 2, name: 'ccc' }
+        ];
 
-//            this.messages;
-            this.deferred = $q.defer();
-
-            this.submit = function () {
-                console.log(this.fromCity);
-                console.log(this.toCity);
-                console.log(this.cities);
-            };
-            mainService.getMessages({from: 'Budapest', to: 'Gyor'}).then(function (data) {
-                /*console.log(data);
-                 $scope.deferred.resolve($scope.messages = data); //was: $defer.resolve($scope.messages = data.result);
-                 */
-                console.log(data);
-            }, function (error) {
-                console.log(error);
-            });
-        }]
+        this.submit = function () {
+            console.log(this.fromCity);
+            console.log(this.toCity);
+            console.log(this.cities);
+        };
+        MainService.getMessages({from: 'Budapest', to: 'Gyor'}).then(function (data) {
+            /*console.log(data);
+             $scope.deferred.resolve($scope.messages = data); //was: $defer.resolve($scope.messages = data.result);
+             */
+            console.log(data);
+        }, function (error) {
+            console.log(error);
+        });
+    }]
 );
