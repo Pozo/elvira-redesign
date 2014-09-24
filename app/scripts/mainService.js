a2d044d7d1b4068f8463e3e63bdc8f65f4f3b7ba
Csaba Valyi <csaba.valy1@gmail.com>
2014-09-24 21:15:50 +0200

    using the minify-safe format in mainService.js

--- app/scripts/mainService.js
+++ app/scripts/mainService.js
@@ -1,13 +1,15 @@
-myApp.service('mainService', function ($resource) {
-    var apiURL = 'http://apiv2.oroszi.net/elvira';
-    var api = $resource(apiURL);
-
-    return {
-        getMessages: function (params) {
-            var get = api.get(params, function (data) {
-                return data;
-            });
-            return get.$promise;
-        }
-    };
-});
+angular.module('main')
+    .service('mainService', ['$resource',function ($resource) {
+        var apiURL = 'http://apiv2.oroszi.net/elvira';
+        var api = $resource(apiURL);
+        console.dir(api);
+        return {
+            getMessages: function (params) {
+                var get = api.get(params, function (data) {
+                    return data;
+                });
+                return get.$promise;
+            }
+        };
+    }]
+);
