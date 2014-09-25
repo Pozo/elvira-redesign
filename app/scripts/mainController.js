ae4da8177c5ffa0ac258f897bfe4681bd3aec52d
Csaba Valyi <csaba.valy1@gmail.com>
2014-09-25 06:59:14 +0200

    wiring in the MainCtrl's submit function

--- app/scripts/mainController.js
+++ app/scripts/mainController.js
@@ -1,11 +1,13 @@
 'use strict';
 
 angular.module('main')
-.controller('MainCtrl', ['$scope', '$datepicker', 'MainService',
-    function ($scope, $datepicker, MainService) {
+.controller('MainCtrl', ['$scope', 'MainService',
+    function ($scope, MainService) {
+
         this.selectedDate = new Date();
 
         this.cities = mav;
+        this.messages = {};
 
         this.discounts = [
             { group : '50%os',id : 1, name: 'asd' },
@@ -13,11 +15,19 @@ angular.module('main')
             { group : '90%os',id : 2, name: 'ccc' }
         ];
 
-        this.submit = function () {
-            console.log(this.fromCity);
-            console.log(this.toCity);
-            console.log(this.cities);
+        this.submit = function (from, to) {
+            var that = this;
+            MainService.getMessages({from: from, to: to}).then(function (data) {
+                console.log(data);
+                that.messages = data;
+            }, function (error) {
+                console.log(error);
+            });
+//            console.log(this.fromCity);
+//            console.log(this.toCity);
+//            console.log(this.cities);
         };
+
         MainService.getMessages({from: 'Budapest', to: 'Gyor'}).then(function (data) {
             /*console.log(data);
              $scope.deferred.resolve($scope.messages = data); //was: $defer.resolve($scope.messages = data.result);
